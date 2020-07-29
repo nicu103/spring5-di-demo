5fded2f1c804eb5a5b6bdafef17fb192f34dafee
nicu <nicu103@nomail.com>
2020-07-29 22:58:13 +0300

    Uses multpile property files

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,16 +1,22 @@
 package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+// @PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
     @Autowired
     Environment env;
@@ -21,6 +27,12 @@ public class PropertyConfig {
     String password;
     @Value("${guru.dburl}")
     String url;
+    @Value("${guru.jms.username}")
+    String jmsUsername;
+    @Value("${guru.jms.password}")
+    String jmsPassword;
+    @Value("${guru.jms.url}")
+    String jmsUrl;
 
     @Bean
     public FakeDataSource fakeDataSource() {
@@ -30,6 +42,16 @@ public class PropertyConfig {
         fakeDataSource.setUrl(url);
         return  fakeDataSource;
     }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUsername(jmsUsername);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUrl(jmsUrl);
+        return  fakeJmsBroker;
+    }
+
     @Bean
     public static PropertySourcesPlaceholderConfigurer properties() {
         return new PropertySourcesPlaceholderConfigurer();
